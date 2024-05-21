bcc0c0819f34e9543f549af838b4ed5fe5a65393
RemxD9 <vladislav.iatsuk@urfu.me>
2024-05-21 14:42:23 +0500

    Шаблон приложения

---
+++ Models_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def main_page():
+    pass
+
+
+def download_dataset():
+    pass
+
+
+def train():
+    pass
+
+
+def results():
+    pass
