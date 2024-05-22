cda750653543d82dc9d739235084140de678753e
RemxD9 <vladislav.iatsuk@urfu.me>
2024-05-23 00:49:21 +0500

    first try

--- Models_app/forms.py
+++ Models_app/forms.py
@@ -3,5 +3,4 @@ from .models import UploadedImage
 
 
 class UploadForm(forms.Form):
-    image_file = forms.FileField(label='Select an image file', required=False)
     nifti_file = forms.FileField(label='Select a NIfTI file', required=False)
--- Models_app/views.py
+++ Models_app/views.py
@@ -17,23 +17,13 @@ def main_page(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
-            image_file = form.cleaned_data['image_file']
             nifti_file = form.cleaned_data['nifti_file']
-
-            if image_file:
-                file_path = 'staticfiles/input/uploaded_image.png'
-                with open(file_path, 'wb') as f:
-                    for chunk in image_file.chunks():
-                        f.write(chunk)
-                return redirect('watching_photos')
             if nifti_file:
                 file_path = 'staticfiles/input/uploaded_image.nii'
                 with open(file_path, 'wb') as f:
                     for chunk in nifti_file.chunks():
                         f.write(chunk)
                 img_3d = nib.load(file_path).get_fdata().transpose(2, 0, 1)
-                mean = -644.2137072615333
-                std = 673.311976351113
                 trans = A.Compose([A.Resize(height=256, width=256), ToTensorV2()])
                 for idx, img_slice in enumerate(img_3d):
                     img_feature = np.clip(img_slice, 0, 1)
@@ -66,23 +56,48 @@ def predict(request):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = load_model(device)
     model.eval()
-    model.to(device)
-    _, transform_val = augmentation()
-    image_path = 'staticfiles/input/uploaded_image.png'
-    image = Image.open(image_path).convert("RGB")
-    image_np = np.array(image)
-    transformed = transform_val(image=image_np)
-    image_tensor = transformed['image']
-    image_tensor = image_tensor.unsqueeze(0)
-    with torch.no_grad():
-        output = model(image_tensor)
 
-    output_image = output.squeeze(0).permute(1, 2, 0).cpu().numpy()
+    _, transform_val = augmentation()
+
+    image_path = 'staticfiles/input/uploaded_image.nii'
+    img_3d = nib.load(image_path).get_fdata().transpose(2, 0, 1)
+
+    mean = -644.2137072615333
+    std = 673.311976351113
+    img_3d = img_3d[img_3d.shape[0] // 2]
+    img_3d = (img_3d - mean) / std
+
+    output_slices = []
+
+    trans = A.Compose([
+        A.Resize(height=256, width=256),
+        ToTensorV2()
+    ])
+
+    for img_slice in img_3d:
+        print(img_slice)
+        img_slice = np.expand_dims(img_slice, axis=0)
+        img_tensor = trans(image=img_slice)['image'].unsqueeze(0)
+        img_tensor = img_tensor.float()
+
+        with torch.no_grad():
+            print('Пропуск изображения')
+            output = model(img_tensor.to(device))
+
+        output_slices.append(output.squeeze(0).cpu().numpy())
+
+    output_3d = np.stack(output_slices, axis=0)
+    output_dir = os.path.join('staticfiles', 'outputs')
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_image = np.mean(output_3d, axis=0).astype(np.uint8)
+    output_image = (255 * (output_image - output_image.min()) / (output_image.max() - output_image.min())).astype(
+        np.uint8)
     output_image_pil = Image.fromarray(output_image)
-    output_image_pil.save('staticfiles/outputs/pred.png')
+    output_image_pil.save(os.path.join(output_dir, 'pred.png'))
     return redirect('results')
 
 
 def results(request):
-    # показываем графики метрик, индекс жакара, можно также картинки(ориг изображение, метку и предикт)
-    pass
+    output_image_path = 'outputs/pred.png'
+    return render('result.html', context={'output_image_path': output_image_path})
