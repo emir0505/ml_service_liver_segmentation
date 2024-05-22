da7e5192f4463715754212d9cdd0112812a25ee5
RemxD9 <vladislav.iatsuk@urfu.me>
2024-05-22 12:22:44 +0500

    + urls

--- ml_service/urls.py
+++ ml_service/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from Models_app.views import main_page, watching_photos
+from Models_app.views import main_page, watching_photos, predict, results
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_page),
-    path('watch/', watching_photos, name='watching_photos')
+    path('watch/', watching_photos, name='watching_photos'),
+    path('predict/', predict, name='predict'),
+    path('results/', results, name='results')
 ]
