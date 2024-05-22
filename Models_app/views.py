f21ed40b564b62514cffdaacebddf058c7bc20b1
RemxD9 <vladislav.iatsuk@urfu.me>
2024-05-22 16:54:32 +0500

    еуые

--- Models_app/views.py
+++ Models_app/views.py
@@ -31,7 +31,7 @@ def watching_photos(request):
     return render(request, 'watching.html', context={'images': image})
 
 
-def load_model(deviпввпрce):
+def load_model(device):
     model = UNET()
     model_path = os.path.join(BASE_DIR, 'models', 'unet_tumor_08.pth')
     model.load_state_dict(torch.load(model_path, map_location=device))
