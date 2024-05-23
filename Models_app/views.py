72d56cb4c6e4b72dc74c128fa35086a792e1fa54
RemxD9 <vladislav.iatsuk@urfu.me>
2024-05-23 17:52:55 +0500

    готово, без стилей

--- Models_app/views.py
+++ Models_app/views.py
@@ -1,12 +1,15 @@
 from PIL import Image
 import numpy as np
 import torch
+from matplotlib import pyplot as plt
+from torchmetrics import JaccardIndex
+from tqdm import tqdm
 from django.shortcuts import redirect, render
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import nibabel as nib
 from Models_app.forms import UploadForm
-from Models_app.models import augmentation, UNET
+from Models_app.models import UNET
 import os
 
 
@@ -52,52 +55,57 @@ def load_model(device):
     return model
 
 
+def load_data_nib(path_features):
+    features = []
+
+    trans_standart = A.Compose([
+        A.Resize(height=256, width=256),
+    ])
+
+    mean = -644.2137072615333
+    std = 673.311976351113
+
+    img_3d_feature = nib.load(path_features).get_fdata().transpose(2, 0, 1)
+
+    for img_feature in img_3d_feature:
+        img_feature = (img_feature - mean) / std
+        trans = trans_standart(image=img_feature)
+        img_feature_ = np.array(trans['image'])
+
+        features.append(img_feature_)
+
+    return torch.tensor(np.array(features)).unsqueeze(1).float()
+
+
 def predict(request):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = load_model(device)
     model.eval()
-
-    _, transform_val = augmentation()
-
     image_path = 'staticfiles/input/uploaded_image.nii'
-    img_3d = nib.load(image_path).get_fdata().transpose(2, 0, 1)
-
-    mean = -644.2137072615333
-    std = 673.311976351113
-    img_3d = img_3d[img_3d.shape[0] // 2]
-    img_3d = (img_3d - mean) / std
-
-    output_slices = []
-
-    trans = A.Compose([
-        A.Resize(height=256, width=256),
-        ToTensorV2()
-    ])
-
-    for img_slice in img_3d:
-        print(img_slice)
-        img_slice = np.expand_dims(img_slice, axis=0)
-        img_tensor = trans(image=img_slice)['image'].unsqueeze(0)
-        img_tensor = img_tensor.float()
-
+    data = load_data_nib(image_path)
+    preds = []
+    cnt = 0
+    for x in tqdm(data):
+        cnt += 1
         with torch.no_grad():
-            print('Пропуск изображения')
-            output = model(img_tensor.to(device))
+            pred = model(x.unsqueeze(0).to(device)).squeeze(0).max(dim=0)[1].cpu()
+        preds.append(pred)
 
-        output_slices.append(output.squeeze(0).cpu().numpy())
-
-    output_3d = np.stack(output_slices, axis=0)
-    output_dir = os.path.join('staticfiles', 'outputs')
-    os.makedirs(output_dir, exist_ok=True)
-
-    output_image = np.mean(output_3d, axis=0).astype(np.uint8)
-    output_image = (255 * (output_image - output_image.min()) / (output_image.max() - output_image.min())).astype(
-        np.uint8)
-    output_image_pil = Image.fromarray(output_image)
-    output_image_pil.save(os.path.join(output_dir, 'pred.png'))
+    cnt = 0
+    for x, y in zip(data, preds):
+        cnt += 1
+        if cnt < 60:
+            continue
+        x = x.squeeze(0).cpu()
+        y = y.squeeze(0).cpu()
+        fig, ax = plt.subplots(1, 2)
+        ax[0].imshow(x)
+        ax[1].imshow(y)
+        plt.savefig('staticfiles/outputs/pred.png')
     return redirect('results')
 
 
 def results(request):
     output_image_path = 'outputs/pred.png'
-    return render('result.html', context={'output_image_path': output_image_path})
+    return render(request, 'result.html', context={'output_image_path': output_image_path})
+
